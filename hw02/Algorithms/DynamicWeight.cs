54b52f5421514fde7ca6d2665d02608ee5bc47a5
Vojtěch Mráz <Zeelize@users.noreply.github.com>
2018-10-23 22:56:31 +0200

    Implemented dynamic algorithm with weight composition

---
+++ hw02/Algorithms/DynamicWeight.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using paa_hw2.Structs;
+
+namespace paa_hw2.Algorithms
+{
+    public class DynamicWeight
+    {
+        private readonly List<Tuple<int, int>> _items;
+        private readonly int _numItems;
+        private readonly int _capacity;
+        private int[,] _array;
+
+        public int BestPrice;
+        
+        public DynamicWeight(Instance inst)
+        {
+            // Fill variables with example data
+            _items = inst.Items;
+            _numItems = inst.NumberOfItems;
+            _capacity = inst.Capacity;
+            
+            // Compute bag with dynamic programming - decomposition via Price
+            // Create 2D array with number of rows = allPrices and columns = number of items
+            // each array item contains minimum weight for the price
+            _array = new int[_capacity + 1, _numItems + 1];
+            
+            StartAlgorithm();
+
+            BestPrice = _array[_capacity, _numItems];
+        }
+
+        private void StartAlgorithm()
+        {
+            for (var m = 0; m <= _capacity; m++)
+            {
+                for (var i = 0; i <= _numItems; i++)
+                {
+                    // Check trivial
+                    if (m == 0 || i == 0) continue;
+
+                    var item = _items[i - 1];
+                    
+                    // tmp index
+                    var tmp = m - item.Item1;
+                    var c1 = _array[m, i - 1];
+                    var cn = item.Item2;
+
+                    // if negative = does not fit -> just put it c1
+                    if (tmp < 0)
+                    {
+                        _array[m, i] = c1;
+                        continue;
+                    }
+
+                    var c2 = _array[tmp, i - 1];
+
+                    // c2 + cn > c1
+                    if (c2 + cn > c1)
+                    {
+                        _array[m, i] = c2 + cn;
+                        continue;
+                    }
+
+                    _array[m, i] = c1;
+                }
+            }
+        }
+    }
+}
--- hw02/Program.cs
+++ hw02/Program.cs
@@ -44,13 +44,17 @@ namespace paa_hw2
             }
 
             // ReSharper disable once InconsistentNaming
-            const int NUMBER_OF_RUNS = 200;
+            const int NUMBER_OF_RUNS = 2000;
             var allBfTimes = 0.0;
             var allBbTimes = 0.0;
-
+            var allDynamicPriceTimes = 0.0;
+            var allDynamicWeightTimes = 0.0;
+            
             // Prepare Lists for best prices
             var bfPrices = new List<int>();
-            var pwPrices = new List<int>();
+            var bbPrices = new List<int>();
+            var dynamicPricePrices = new List<int>();
+            var dynamicWeightPrices = new List<int>();
 
             for (var i = 0; i < NUMBER_OF_RUNS; i++)
             {
@@ -66,8 +70,7 @@ namespace paa_hw2
                     if (i == 0) bfPrices.Add(bruteForce.BestPrice);
                 }
                 watchBf.Stop();
-                var bfTime = watchBf.Elapsed.TotalMilliseconds;
-                allBfTimes += bfTime;
+                allBfTimes += watchBf.Elapsed.TotalMilliseconds;
                 //*******************************************************
 
                 // Branch and Bound algorithm
@@ -75,24 +78,48 @@ namespace paa_hw2
                 watchBb.Start();
                 foreach (var inst in instances)
                 {
+                    continue;
                     var branchBound = new BbMethod(inst);
-                    if (i == 0) pwPrices.Add(branchBound.BestPrice);
+                    if (i == 0) bbPrices.Add(branchBound.BestPrice);
                 }
                 watchBb.Stop();
-                var bbTime = watchBb.Elapsed.TotalMilliseconds;
-                allBbTimes += bbTime;
+                allBbTimes += watchBb.Elapsed.TotalMilliseconds;
                 //*******************************************************
                 
-                // TODO Dynamic algorithm
-                
+                // Dynamic Price algorithm
+                var watchDynamicPrice = new Stopwatch();
+                watchDynamicPrice.Start();
+                foreach (var inst in instances)
+                {
+                    continue;
+                    var dynamic = new DynamicPrice(inst);
+                    if (i == 0) dynamicPricePrices.Add(dynamic.BestPrice);
+                }
+                watchDynamicPrice.Stop();
+                allDynamicPriceTimes += watchDynamicPrice.Elapsed.TotalMilliseconds;
                 //*******************************************************
                 
+                // Dynamic Weight algorithm
+                var watchDynamicWeight = new Stopwatch();
+                watchDynamicWeight.Start();
+                foreach (var inst in instances)
+                {
+                    var dynamic = new DynamicWeight(inst);
+                    if (i == 0) dynamicWeightPrices.Add(dynamic.BestPrice);
+                }
+                watchDynamicWeight.Stop();
+                allDynamicWeightTimes += watchDynamicWeight.Elapsed.TotalMilliseconds;
+                //*******************************************************
+
                 // TODO FPTAS algorithm
             }
-
-            Console.WriteLine("\nNumber of runs: " + NUMBER_OF_RUNS);
-            //Console.WriteLine("BF Avg Time: " + allBfTimes / NUMBER_OF_RUNS);
+            
+            Console.WriteLine("\n" + args[0]);
+            Console.WriteLine("Number of runs: " + NUMBER_OF_RUNS);
+            Console.WriteLine("BF Avg Time: " + allBfTimes / NUMBER_OF_RUNS);
             Console.WriteLine("BB Avg Time: " + allBbTimes / NUMBER_OF_RUNS);
+            Console.WriteLine("Dynamic Price Avg Time: " + allDynamicPriceTimes / NUMBER_OF_RUNS);
+            Console.WriteLine("Dynamic Weight Avg Time: " + allDynamicWeightTimes / NUMBER_OF_RUNS);
         }
     }
 }
